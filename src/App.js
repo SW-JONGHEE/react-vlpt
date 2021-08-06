d52513ce33152f862f18b858edd694ce12b8b1da
jonghee <jonghee@nextculture.kr>
2021-08-06 13:54:07 +0900

    배열에 항목 제거하기

--- src/App.js
+++ src/App.js
@@ -72,6 +72,10 @@ function App() {
     nextId.current += 1;
   };
 
+  const onRemove = (id) => {
+    setUsers(users.filter(user => user.id !== id ));
+  };
+
   return (
     <Wrapper>
         <Hello name="react" color="red" isSpecial={true}/>
@@ -89,7 +93,7 @@ function App() {
         <List />
 
         <div style={style}>UserList.js</div>
-        <UserList users={users}/>
+        <UserList users={users} onRemove={onRemove}/>
 
         <div style={style}>CreateUser.js</div>
         <CreateUser
@@ -98,7 +102,7 @@ function App() {
             onChange={onChange}
             onCreate={onCreate}
         />
-        <UserList users={users}/>
+        <UserList users={users} onRemove={onRemove}/>
     </Wrapper>
   );
 }
