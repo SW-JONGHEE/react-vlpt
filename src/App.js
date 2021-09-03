ad94a310ad1a97d13d08afc4bc56cbf4e0ad4e20
jonghee <jonghee@nextculture.kr>
2021-09-03 14:48:43 +0900

    23. Immer를 사용한 더 쉬운 불변성 관리 - Immer 라이브러리

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import UserList from "./UserList";
 import React, {useCallback, useMemo, useReducer, useRef, useState} from "react";
 import CreateUser from "./CreateUser";
 import useInputs from "./hooks/useInputs";
+import produce from "immer";
 
 function countActiveUsers(users) {
     console.log('활성 사용자 수를 세는중');
@@ -55,32 +56,44 @@ function reducer (state, action) {
         //         }
         //     };
         case 'CREATE_USER':
-            return {
-                inputs: initialState.inputs,
-                users: state.users.concat(action.user),
-            };
-
+            // return {
+            //     inputs: initialState.inputs,
+            //     users: state.users.concat(action.user),
+            // };
+            return produce(state, draft => {
+                draft.users.push(action.user);
+            });
         case 'TOGGLE_USER':
-            return {
-                /*
-                * users => users.map(user =>
-                   user.id === id ? {...user, active: !user.active} : user
-                 )
-                * */
+            // return {
+            //     /*
+            //     * users => users.map(user =>
+            //        user.id === id ? {...user, active: !user.active} : user
+            //      )
+            //     * */
+            //
+            //     ...state,
+            //     users: state.users.map(user => user.id === action.id ? {
+            //         ...user,
+            //         active: !user.active
+            //     } : user),
+            // };
 
-                ...state,
-                users: state.users.map(user => user.id === action.id ? {
-                    ...user,
-                    active: !user.active
-                } : user),
-            };
+            return produce(state, draft => {
+                const user = draft.users.find(user => user.id === action.id);
+                user.active = !user.active;
+            });
 
         case 'REMOVE_USER':
-            return {
-                //users.filter(user => user.id !== id )
-                ...state,
-                users: state.users.filter(user => user.id !== action.id),
-            }
+            // return {
+            //     //users.filter(user => user.id !== id )
+            //     ...state,
+            //     users: state.users.filter(user => user.id !== action.id),
+            // }
+
+            return produce(state, draft => {
+                const index = draft.users.findIndex(user => user.id === action.id);
+                draft.users.splice(index, 1);
+            });
 
         default:
             return state;
